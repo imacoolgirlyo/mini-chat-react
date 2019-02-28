4b7e793057877931cd18fa4c502c5ef32a012dd4
imacoolgirlyo <tjrud1438@naver.com>
2019-03-01 00:11:39 +0900

    userController, chatController 만들고 Router와 연결

---
+++ src/controllers/chatController.js
@@ -0,0 +1,2 @@
+export const home = (req, res) => res.send('home');
+export const hi = (req, res) => res.send('hi');
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import routes from '../routes';
+import {home} from '../controllers/chatController';
+import {
+    join,
+    login,
+    logout
+} from '../controllers/userController';
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send("Home"));
-globalRouter.get(routes.join, (req, res) => res.send("JOIN"));
-globalRouter.get(routes.login, (req, res) => res.send("Login"));
-globalRouter.get(routes.logout, (req, res) => res.send("Logout"
-));
+console.log(home);
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
