99cb4e4b6d857810f1a1f490f3ede920cdef3e9f
imacoolgirlyo <tjrud1438@naver.com>
2019-03-05 22:43:37 +0900

    middleware(routes, siteName) 추가하여 pug에siteName과routes들  local 변수 적용

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { localsMiddleware } from './middlewares';
+import routes from './routes';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
-import routes from './routes';
+
 import path from "path";
 
 const app = express();
@@ -14,11 +16,13 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
-app.use(helmet());
+
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 
 app.use(routes.users, userRouter);
